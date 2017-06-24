9e12d8394d424d708933fbae6aadef0483c41c02
Cristian Madrid <cristianmadridd@gmail.com>
2017-06-24 16:37:26 -0300

    refactor: add support to cpf and cnpj

--- GeneralFormatter/Classes/CPFFormatter.swift
+++ GeneralFormatter/Classes/CPFFormatter.swift
@@ -1,5 +1,5 @@
 //
-//  CPFFormatt.swift
+//  CPFFormatter.swift
 //  Pods
 //
 //  Created by Cristian Madrid on 22/06/17.
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+struct CPFFormatter: Formatter {
+    
+    func format(value: String) -> String {
+        let valueWithoutCharacters = removeSpecialCharacters(of: value)
+        let formattedString = NSMutableString(string: valueWithoutCharacters)
+        let count = valueWithoutCharacters.characters.count
+        
+        if count > 2 {
+            formattedString.insert(".", at: 3)
+        }
+        if count > 5 {
+            formattedString.insert(".", at: 7)
+        }
+        if count > 8 {
+            formattedString.insert("-", at: 11)
+        }
+        return formattedString as String
+    }
+
+    func shouldChangeCharacters(of textField: UITextField, inRange range: NSRange) -> Bool {
+        return shouldChangeCharacters(of: textField, inRange: range, withLimit: 14)
+    }
+}
--- GeneralFormatter/Classes/Formatter.swift
+++ GeneralFormatter/Classes/Formatter.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+protocol Formatter {
+    func format(value: String) -> String
+    func shouldChangeCharacters(of textField: UITextField, inRange range: NSRange) -> Bool
+}
+
+extension Formatter {
+    func removeSpecialCharacters(of value: String) -> String {
+        var string = value.replacingOccurrences(of: ".", with: "")
+        string = string.replacingOccurrences(of: "/", with: "")
+        string = string.replacingOccurrences(of: "-", with: "")
+        string = string.replacingOccurrences(of: "(", with: "")
+        string = string.replacingOccurrences(of: ")", with: "")
+        string = string.replacingOccurrences(of: " ", with: "")
+        
+        return string
+    }
+    
+    func shouldChangeCharacters(of textField: UITextField, inRange range: NSRange, withLimit limit: Int) -> Bool {
+        let text = textField.text ?? ""
+        guard text.characters.count < limit else {
+            if range.location < limit {
+                return true
+            }
+            return false
+        }
+        if range.location >= text.characters.count {
+            textField.text = format(value: text)
+        }
+        return true
+    }
+}
