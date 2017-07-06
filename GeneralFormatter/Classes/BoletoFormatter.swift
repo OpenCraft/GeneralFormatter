f8cd1f354b8d67fa2cc6a31c9dbe680df990ba2e
rottajuliano <juliano.rotta@ilegra.com>
2017-07-06 15:35:39 -0300

    Added BoletoFormatter

---
+++ GeneralFormatter/Classes/BoletoFormatter.swift
@@ -0,0 +1,37 @@
+//
+//  BoletoFormatter.swift
+//  Pods
+//
+//  Created by Juliano Rotta on 06/07/17.
+//
+//
+
+import Foundation
+
+struct BoletoFormatter: Formatter {
+    
+    func format(value: String) -> String {
+        let valueWithoutCharacters = removeSpecialCharacters(of: value)
+        let formattedString = NSMutableString(string: valueWithoutCharacters)
+        let count = valueWithoutCharacters.characters.count
+        
+        if count > 4 {
+            formattedString.insert(".", at: 5)
+        }
+        if count > 10 {
+            formattedString.insert(" ", at: 11)
+        }
+        if count > 16 {
+            formattedString.insert(".", at: 17)
+        }
+        if count > 23 {
+            formattedString.insert(" ", at: 24)
+        }
+        return formattedString as String
+    }
+    
+    func shouldChangeCharacters(of textField: UITextField, inRange range: NSRange) -> Bool {
+        return shouldChangeCharacters(of: textField, inRange: range, withLimit: 54)
+    }
+}
+
