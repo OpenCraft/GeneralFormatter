996831b733849f7fe88fec57560056ce4992f4b9
Luis Filipe Campani <luis.silva@ilegra.com>
2017-10-03 16:24:19 -0300

    Split barcode and covenant in two different classes and created a class that manages which one should be used.

---
+++ GeneralFormatter/Classes/BoletoConvenioFormatter.swift
@@ -0,0 +1,31 @@
+//
+//  BoletoConvenioFormatter.swift
+//  GeneralFormatter
+//
+//  Created by Luis Filipe Campani on 10/3/17.
+//
+
+import Foundation
+
+struct BoletoConvenioFormatter: Formatter {
+
+    func format(value: String) -> String {
+        let firstChar = value.digitsOnly.characters.first
+
+        if firstChar == "8" {
+            return ConvenioFormatter().format(value: value)
+        } else {
+            return BoletoFormatter().format(value: value)
+        }
+    }
+
+    func shouldChangeCharacters(of textField: UITextField, inRange range: NSRange, typedText: String) -> Bool {
+        let firstChar = (textField.text ?? "").digitsOnly.characters.first
+
+        if firstChar == "8" {
+            return ConvenioFormatter().shouldChangeCharacters(of: textField, inRange: range, typedText: typedText)
+        } else {
+            return BoletoFormatter().shouldChangeCharacters(of: textField, inRange: range, typedText: typedText)
+        }
+    }
+}
