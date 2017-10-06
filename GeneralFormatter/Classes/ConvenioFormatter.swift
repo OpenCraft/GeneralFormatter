d8bbd96dca392516b8183496a734f1f7f4d9331c
Luis Filipe Campani <luis.silva@ilegra.com>
2017-10-06 16:45:59 -0300

    Updating convene format to “.” Instead of “-”

--- GeneralFormatter/Classes/ConvenioFormatter.swift
+++ GeneralFormatter/Classes/ConvenioFormatter.swift
@@ -14,25 +14,25 @@ struct ConvenioFormatter: CustomFormatter {
         let formattedString = NSMutableString(string: value.digitsOnly)
 
         if formattedString.count > 11 {
-            formattedString.insert("-", at: 11)
+            formattedString.insert(".", at: 11)
         }
         if formattedString.count > 13 {
             formattedString.insert(" ", at: 13)
         }
         if formattedString.count > 25 {
-            formattedString.insert("-", at: 25)
+            formattedString.insert(".", at: 25)
         }
         if formattedString.count > 27 {
             formattedString.insert(" ", at: 27)
         }
         if formattedString.count > 39 {
-            formattedString.insert("-", at: 39)
+            formattedString.insert(".", at: 39)
         }
         if formattedString.count > 41 {
             formattedString.insert(" ", at: 41)
         }
         if formattedString.count > 53 {
-            formattedString.insert("-", at: 53)
+            formattedString.insert(".", at: 53)
         }
 
         return formattedString as String
