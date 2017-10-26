78ad1ca62884927ef5fc3d0deac64814e7ff6743
Cristian Madrid <cristianmadridd@gmail.com>
2017-10-25 23:57:42 -0200

    test: add quick and nimble to do the unit tests. add test for formatt method of CPFFormatter

--- Example/Tests/CPFFormatterTests.swift
+++ Example/Tests/CPFFormatterTests.swift
@@ -1,23 +1,69 @@
 import XCTest
+import Quick
+import Nimble
+
 @testable import GeneralFormatter
 
-class CPFFormatterTests: XCTestCase {
+class CPFFormatterTests: QuickSpec {
 
-    var formatter: CPFFormatter!
+    override func spec() {
 
-    override func setUp() {
-        super.setUp()
-        formatter = CPFFormatter()
-    }
-    
-    override func tearDown() {
-        super.tearDown()
-    }
+        var formatter: CPFFormatter!
 
-    func testFormatCompletedCPF() {
-        let given = formatter.format(value: "12345678900")
-        let expected = "123.456.789-00"
+        beforeEach {
+            formatter = CPFFormatter()
+        }
 
-        assert(given == expected)
+        describe("CPFFormatter") {
+
+            it("should formatt a complete cpf") {
+                let given = formatter.format(value: "12345678900")
+                let expected = "123.456.789-00"
+
+                expect(given).to(equal(expected))
+            }
+
+            it("should'nt add any character") {
+                let given = formatter.format(value: "123")
+                let expected = "123"
+
+                expect(given).to(equal(expected))
+            }
+
+            it("should add `.` when four digits is passed") {
+                let given = formatter.format(value: "1234")
+                let expected = "123.4"
+
+                expect(given).to(equal(expected))
+            }
+
+            it("should'nt add the second `.` when six digits is passed") {
+                let given = formatter.format(value: "123456")
+                let expected = "123.456"
+
+                expect(given).to(equal(expected))
+            }
+
+            it("should add the second `.` when seven digits is passed") {
+                let given = formatter.format(value: "1234567")
+                let expected = "123.456.7"
+
+                expect(given).to(equal(expected))
+            }
+
+            it("should'nt add the third `-` when nine digits is passed") {
+                let given = formatter.format(value: "123456789")
+                let expected = "123.456.789"
+
+                expect(given).to(equal(expected))
+            }
+
+            it("should add the third `-` when ten digits is passed") {
+                let given = formatter.format(value: "1234567890")
+                let expected = "123.456.789-0"
+
+                expect(given).to(equal(expected))
+            }
+        }
     }
 }
